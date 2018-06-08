49d746c490a2d103550d3623691e0058b7ec39af
slepeng <13roman1997@tut.by>
2018-06-08 16:35:42 +0300

    Initial Commit

---
+++ House/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  House
+//
+//  Created by Roman Morozov on 8.06.18.
+//  Copyright © 2018 Udacity. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
